63866cc1b0c019bccb0c9fe78c7f6892c586fc2a
52006 <52006@etu.he2b.be>
2021-10-15 11:24:09 +0200

    First iteration of view

---
+++ resources/views/studentsConsultation.blade.php
@@ -0,0 +1,12 @@
+@extends('canvas')
+@section('title', 'Présence des étudiants au cours')
+@section('content')
+
+<p><div>Matricule</div><div>Nom</div><div>Prénom</div><div>Groupes</div></p>
+<ol id="attendance"> 
+    @foreach ($students as $student)
+    <li>{{$student->getId() . " " .  $student->getLastName() . " " . $student->getFirstName() . " " . $student->getGroups()[0]}}</li>
+    @endforeach
+</ol>
+
+@endsection
