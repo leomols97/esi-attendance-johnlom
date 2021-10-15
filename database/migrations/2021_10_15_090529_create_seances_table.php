4e642a2d2fdfce4f1cf746b3d7279a06cdda0ab7
MESSAST Haïtham <54259@etu.he2b.be>
2021-10-15 12:27:05 +0200

    DB creation & migrations, attendance query

---
+++ database/migrations/2021_10_15_090529_create_seances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seances', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->unsignedInteger('course_id');
+            $table->unsignedInteger('teacher_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seances');
+    }
+}
