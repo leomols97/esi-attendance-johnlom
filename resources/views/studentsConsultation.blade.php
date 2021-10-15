23da25dbb71c9b6e55179a265522af30e9c7814c
52006 <52006@etu.he2b.be>
2021-10-15 11:35:25 +0200

    Second iteration of view

--- resources/views/studentsConsultation.blade.php
+++ resources/views/studentsConsultation.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Présence des étudiants au cours')
 @section('content')
 
-<p><div>Matricule</div><div>Nom</div><div>Prénom</div><div>Groupes</div></p>
+<p>Structure: <div>Matricule</div><div>Nom</div><div>Prénom</div><div>Groupes</div></p>
 <ol id="attendance"> 
     @foreach ($students as $student)
     <li>{{$student->getId() . " " .  $student->getLastName() . " " . $student->getFirstName() . " " . $student->getGroups()[0]}}</li>
