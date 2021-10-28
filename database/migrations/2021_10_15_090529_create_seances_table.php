c505aa5cc80f67efc01a5e208cf1ab5ebe2ec169
Jan Schumacher Vincke <53135@etu.he2b.be>
2021-10-28 16:01:22 +0200

    Add migrations #13

---
+++ database/migrations/2021_10_15_090529_create_seances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seances', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->unsignedInteger('course_ue');
+            $table->unsignedInteger('students_list');
+            $table->unsignedInteger('teacher_acronym');
+            $table->unsignedInteger('room');
+            $table->foreign('course_ue')->references('ue')->on('courses');
+            $table->foreign('teacher_acronym')->references('acronym')->on('teachers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seances');
+    }
+}
---
+++ database/migrations/2021_10_15_094504_create_student_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_groups', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('student_id');
+            $table->unsignedInteger('group_id');
+            //$table->foreign('student_id')->references('students')->on('id');
+            //$table->foreign('group_id')->references('groups')->on('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_groups');
+    }
+}
