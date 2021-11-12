da6ebf52a6d20b1342d2e2ca09154f850ffe86d7
Leopold Mols <53212@etu.he2b.be>
2021-11-12 19:30:29 +0100

    Frontend begun related to #14

---
+++ resources/views/canevas.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/CSS/style.css">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <img id="logo" src="/he2b-esi.jpg" alt="HE2B-ESI"/>
+        <h1>PRJG5 - Attendance</h1>
+        <ul>
+            <li><a href="/addStudentToCourse">Student List</a></li>
+            <!-- <li><a href="/course">Course List</a></li> -->
+        </ul>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,18 +1,50 @@
-<h3>Ajouter</h3>
-<p>Ajouter un étudiant à un cours</p>
+@extends('canevas')
 
-<form method="POST" action="/export">
+@section('title', 'StudentList')
 
-    <!-- {{ csrf_field() }}
+<?php $students = json_decode($students,true); ?>
 
-    <input type="text" name="name" placeholder="Nom de fichier" >
-
-    <select name="extension" >
-        foreach student de la table student, mettre dans lles possibiltés
-        <option value="xlsx" >.xlsx</option>
-        <option value="csv" >.csv</option>
-    </select>
-
-    <button type="submit" >Exporter</button> -->
-
-</form>
+@section ('content')
+    <h1>Student List</h1>
+    @if(count($students)===0)
+        <p>No student</p>
+    @else
+        <table>
+            <tr><th>id</th><th>Last name</th><th>First name</th></tr>
+            @foreach($students as $student)
+                <tr>
+                    <td dusk="id_student"><?=$student["id"]?></td>
+                    <td><?=$student["last_name"]?></td>
+                    <td><?=$student["first_name"]?></td>
+                </tr>
+            @endforeach
+        </table>
+    @endif
+    <div>
+        <form id="add" @submit="checkForm" action="/student/add" method="POST">
+            @csrf
+            <h5>Add a Student:</h5>
+            <p>
+                <label for="id">Id :</label>
+                <input id="id" name="id" type="number" v-model="id">
+            </p>
+            <p>
+                <label for="LastName">Last Name :</label>
+                <input id="LastName" name="last_name" type="text" v-model="LastName">
+            </p>
+            <p>
+                <label for="FirstName">First Name :</label>
+                <input id="FirstName" name="first_name" type="text" v-model="FirstName">
+            </p>
+            <p><input type="submit" value="Add"></p>
+            <div id="error"></div>
+        </form>
+        <form id="delete" action="/student/delete" method="POST">
+            @csrf
+            <h5>Delete a Student:</h5>
+            <label for="id">Id:</label>
+            <input id="id" name="id" type="number">
+            <input type="submit" value=delete>
+        </form>
+    </div>
+@endsection
