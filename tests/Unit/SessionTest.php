8284ec30f5a7ff72bbdcf9bc1954bdc65ac92efa
52006 <52006@etu.he2b.be>
2021-11-18 01:04:01 +0100

    second iteration of backend tests, Related to #16

--- tests/Unit/SessionTest.php
+++ tests/Unit/SessionTest.php
@@ -6,13 +6,22 @@ use PHPUnit\Framework\TestCase;
 
 class SessionTest extends TestCase
 {
+    use RefreshDatabase;
     /**
+     * 
      * A basic unit test example.
      *
      * @return void
      */
     public function test_importICS()
     {
-        $this->assertTrue(true);
+        $ics = ("..\..\public\JLC_LECHIEN_Jonathan.ics");
+        Session::importICS($ics);
+
+        $this->assertDatabaseCount('courses', 100);
+        $this->assertDatabaseCount('teachers', 100);
+        $this->assertDatabaseCount('groups', 100);
+        $this->assertDatabaseCount('courses_groups', 100);
+        $this->assertDatabaseCount('seances', 100);
     }
 }
