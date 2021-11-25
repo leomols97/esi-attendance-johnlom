ac1a2f3b3c6442b018416869e44b4a4bebfe786f
Marika Winska <55047@etu.he2b.be>
2021-11-25 17:01:55 +0100

    Revert "Resolve security issue css"
    This reverts commit 3dca6208de867861d7df2be6e6e174b167d187a3.

--- resources/views/canvas.blade.php
+++ resources/views/canvas.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/canvas.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/canvas.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   @yield('css')
   <title>@yield('title')</title>
