8f4667e7a18be0d4ba2435707b095805edd2454f
leomols97 <leomols@hotmail.com>
2021-11-29 16:14:18 +0100

    Unit tests done related to and closes #29

--- app/Http/Controllers/ExceptionController.php
+++ app/Http/Controllers/ExceptionController.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * The controler to add an exisiting student to an existing course
- *
- * @copyright  53212 (MOLS Léopold) && 53135 (SCHUMACHER VINCKE Jan)
- * @link       https://git.esi-bru.be/prjg5-2021-22/esi-attendance-johnlom
- */
+* The controler to add an exisiting student to an existing course
+*
+* @copyright  53212 ( MOLS Léopold ) && 53135 ( SCHUMACHER VINCKE Jan )
+* @link       https://git.esi-bru.be/prjg5-2021-22/esi-attendance-johnlom
+*/
 
 // namespace App\Exceptions;
 namespace App\Http\Controllers;
@@ -18,54 +18,59 @@ use App\Exceptions;
 
 // use Exception;
 
-class ExceptionController extends Controller
-{
+class ExceptionController extends Controller {
     /**
-     * Sends the students and the courses available to the view that has to show them
-     *
-     * @return void
-     */
-    public function showingStudentAndCourses()
-    {
+    * Sends the students and the courses available to the view that has to show them
+    *
+    * @return void
+    */
+
+    public function showingStudentAndCourses() {
         $students = StudentModel::findAllStudents();
         $courses = StudentModel::findAllCourses();
-        return view('student', ['students' => $students, 'courses' => $courses]);
+        return view( 'student', ['students' => $students, 'courses' => $courses] );
     }
 
     /**
-     * Adds a student to a course and puts it into the table 'exception_student_list'
-     *
-     * @return void
-     */
-    public function addStudentToCourse()
-    {
-        if (isset($_REQUEST["course_id"])
-            && isset($_REQUEST["student_id"]))
-        {
-            $courseId = $_REQUEST["course_id"];
-            $studentId = $_REQUEST["student_id"];
-            StudentModel::addAndUpdateStudentToCourse($courseId, $studentId, true);
+    * Adds a student to a course and puts it into the table 'exception_student_list'
+    *
+    * @return void
+    */
+
+    public function addStudentToCourse() {
+        if ( isset( $_REQUEST['course_id'] )
+        && isset( $_REQUEST['student_id'] ) ) {
+            $courseId = $_REQUEST['course_id'];
+            $studentId = $_REQUEST['student_id'];
+            try {
+                StudentModel::addAndUpdateStudentToCourse( $courseId, $studentId, true );
+            } catch ( \Exception $exception ) {
+                echo $exception->getmessage();
+            }
         } else {
-            echo "<script>alert(\"Veuillez remplir chaque champ pour l'ajout !\")</script>";
+            echo '<script>alert(\'Veuillez remplir chaque champ pour l\'ajout !\' )</script>';
         }
         return $this->showingStudentAndCourses();
     }
 
     /**
      * Deletes a student to a course and puts it into the table 'exception_student_list'
-     *
-     * @return void
-     */
-    public function DeleteStudentFromCourse()
-    {
-        if (isset($_REQUEST["course_id"])
-            && isset($_REQUEST["student_id"]))
+    *
+    * @return void
+    */
+    public function DeleteStudentFromCourse() {
+        if ( isset( $_REQUEST['course_id'] )
+            && isset( $_REQUEST['student_id'] ) )
         {
-            $courseId = $_REQUEST["course_id"];
-            $studentId = $_REQUEST["student_id"];
-            StudentModel::DeleteStudentFromCourse($courseId, $studentId);
+            $courseId = $_REQUEST['course_id'];
+            $studentId = $_REQUEST['student_id'];
+            try {
+                StudentModel::DeleteStudentFromCourse( $courseId, $studentId );
+            } catch (\Exception $exception) {
+                echo $exception->getmessage();
+            }
         } else {
-            echo "<script>alert(\"Veuillez remplir chaque champ pour la suppression !\")</script>";
+                echo '<script>alert( \'Veuillez remplir chaque champ pour la suppression !\' )</script>';
         }
         return $this->showingStudentAndCourses();
     }
--- app/Models/StudentModel.php
+++ app/Models/StudentModel.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * The queries model to add an exisiting student to an existing course
- *
- * @copyright  53212 (MOLS Léopold) && 53135 (SCHUMACHER VINCKE Jan)
- * @link       https://git.esi-bru.be/prjg5-2021-22/esi-attendance-johnlom
- */
+* The queries model to add an exisiting student to an existing course
+*
+* @copyright  53212 ( MOLS Léopold ) && 53135 ( SCHUMACHER VINCKE Jan )
+* @link       https://git.esi-bru.be/prjg5-2021-22/esi-attendance-johnlom
+*/
 
 namespace App\Models;
 
@@ -13,150 +13,137 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\QueryException;
+use Exception;
 
-class StudentModel extends Model
-{
+class StudentModel extends Model {
     use HasFactory;
 
     /**
-     * Finds all students in the table "students"
-     *
-     * @return void
-     */
-    static public function findAllStudents()
-    {
-        $students = DB::select('
+    * Finds all students in the table 'students'
+    *
+    * @return void
+    */
+    static public function findAllStudents() {
+        $students = DB::select( '
             SELECT id, last_name, first_name
             FROM students
             ORDER BY id ASC
-        ');
+        ' );
         return $students;
     }
 
     /**
-     * Finds all courses in the table "courses"
-     *
-     * @return void
-     */
-    static public function findAllCourses()
-    {
-        $students = DB::select('
+    * Finds all courses in the table 'courses'
+    *
+    * @return void
+    */
+    static public function findAllCourses() {
+        $students = DB::select( '
             SELECT id, name, teacher_id
             FROM courses
             ORDER BY id ASC
-        ');
+        ' );
         return $students;
     }
 
     /**
-     * Adds or updates a student to a course belonging the course_id and the student_id
-     *
-     * @param  integer $course_id     The id of the course to add the student to
-     * @param  integer $student_id    The id of the student to add to a course
-     * @param  boolean $add           The status of the adding
-     *
-     * @throws QueryException if the query could not be executed
-     *
-     * @return void
-     */
-    static public function addAndUpdateStudentToCourse($course_id, $student_id, $add)
-    {
+    * Adds or updates a student to a course belonging the course_id and the student_id
+    *
+    * @param  integer $course_id     The id of the course to add the student to
+    * @param  integer $student_id    The id of the student to add to a course
+    * @param  boolean $add           The status of the adding
+    *
+    * @throws QueryException if the query could not be executed
+    *
+    * @return void
+    */
+    static public function addAndUpdateStudentToCourse( $course_id, $student_id, $add ) {
         // Looks in the table 'exception_student_list' if the student
         // given in the parameters is already added with the course given in the parameters
-        $student = DB::select(
-            'SELECT course_id, student_id
+        $student = DB::select('
+            SELECT course_id, student_id
             FROM exception_student_list
             WHERE course_id = ? AND student_id = ?
             ',
             [$course_id, $student_id]
         );
-        try {
-            if (empty($student))
-            {
-                // Inserts the student given as parameter in the table 'exception_student_list'
-                // with the course givent as parameters
-                DB::table('exception_student_list')->updateOrInsert([
-                    'course_id' => $course_id,
-                    'student_id' => $student_id,
-                ], ['add' => $add]);
-            } else {
-                echo "<script>alert(\"L'étudiant a déjà été ajouté à ce cours\")</script>";
-            }
-        } catch (QueryException $exception) {
-            throw $exception;
+        if ( empty( $student ) ) {
+            // Inserts the student given as parameter in the table 'exception_student_list'
+            // with the course givent as parameters
+            DB::table( 'exception_student_list' )->updateOrInsert( [
+                'course_id' => $course_id,
+                'student_id' => $student_id,
+            ], ['add' => $add] );
+        } else {
+            throw new Exception( "L'étudiant a déjà été ajouté à ce cours !" );
         }
     }
 
     /**
-     * Selects a student
-     *
-     * @param  integer $id  The id of the student to select
-     *
-     * @return void
-     */
-    static public function selectStudent($id)
-    {
-        return DB::table('students')
-                ->select('id')
-                ->where('id', '=', $id)
-                ->get();
+    * Selects a student
+    *
+    * @param  integer $id  The id of the student to select
+    *
+    * @return void
+    */
+    static public function selectStudent( $id ) {
+        return DB::table( 'students' )
+        ->select( 'id' )
+        ->where( 'id', ' = ', $id )
+        ->get();
     }
 
     /**
-     * Selects a course
-     *
-     * @param  integer $id  The id of the course to select
-     *
-     * @return void
-     */
-    static public function selectCourse($id)
-    {
-        return DB::select('select id from courses where id = ?', [$id]);
+    * Selects a course
+    *
+    * @param  integer $id  The id of the course to select
+    *
+    * @return void
+    */
+    static public function selectCourse( $id ) {
+        return DB::select( 'select id from courses where id = ?', [$id] );
     }
 
     /**
-     * Selects all the elements from the table 'exception_student_list'
-     *
-     * @return all the elements of the table
-     */
-    public static function selectAllExceptions()
-    {
-        return DB::table('exception_student_list')->get();
+    * Selects all the elements from the table 'exception_student_list'
+    *
+    * @return all the elements of the table
+    */
+    public static function selectAllExceptions() {
+        return DB::table( 'exception_student_list' )->get();
     }
 
     /**
-     * Deletes a student from the table 'exception_student_list'
-     *
-     * @param  integer $course_id   The id of the course to add to the table
-     * @param  integer $student     The id of the student to add to the table
-     *
-     * @return void
-     */
-    public static function DeleteStudentFromCourse($course_id, $student_id)
-    {
-        // Sélectionne le tuple dont l'étudiant et le cours sont donnés en paramètres
-        // pour voir s'il existe ou non
-        $student = DB::select(
-            'SELECT course_id, student_id
+    * Deletes a student from the table 'exception_student_list'
+    *
+    * @param  integer $course_id   The id of the course to add to the table
+    * @param  integer $student     The id of the student to add to the table
+    *
+    * @return void
+    */
+    public static function DeleteStudentFromCourse( $course_id, $student_id ) {
+        // Looks in the table 'exception_student_list' if the student
+        // given in the parameters is already added with the course given in the parameters
+        $student = DB::select('
+            SELECT course_id, student_id
             FROM exception_student_list
             WHERE course_id = ? AND student_id = ?
             ',
             [$course_id, $student_id]
         );
-        try {
-            if (!empty($student))
-            {
-                // Deletes a student from the table 'exception_student_list'
-                // with the parameters of the function
-                DB::table('exception_student_list')
-                ->where('course_id', '=', $course_id)
-                ->where('student_id', '=', $student_id)
-                ->delete();
-            } else {
-                echo "<script>alert(\"L'étudiant n'a pas encore été ajouté à ce cours\")</script>";
-            }
-        } catch (QueryException $exception) {
-            throw $exception;
+        if ( !empty( $student ) ) {
+            // Deletes a student from the table 'exception_student_list'
+            // with the parameters of the function
+            DB::delete('
+                DELETE
+                FROM exception_student_list
+                WHERE course_id = ? AND student_id = ?
+            ',
+            [$course_id, $student_id]
+        );
+        } else {
+            // echo '<script>alert(\'L'étudiant n'a pas encore été ajouté à ce cours !\')</script>';
+            throw new Exception( "L'étudiant n'a pas encore été ajouté à ce cours !" );
         }
     }
 }
