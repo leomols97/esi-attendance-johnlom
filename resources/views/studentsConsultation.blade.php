675a7988d4432f71541f08d3838f651b435f8b81
Nathan Descheemeaeker <49880@etu.he2b.be>
2021-12-12 20:24:12 +0100

    Closes #34; Add search bar

--- resources/views/studentsConsultation.blade.php
+++ resources/views/studentsConsultation.blade.php
@@ -4,23 +4,24 @@
 @section('content')
 @section('css')
 @section('content')
+    <head>
+        <link rel="stylesheet" type="text/css" href="{{secure_asset('css/studentConsultation.css') }}">
+        <script type="text/javascript" src="{{secure_asset('js/presence_taking.js')}}"></script>
+    </head>
 
     <h1>Les étudiants</h1>
 
+    <input type="text" id="myInput" onkeyup="searchingInDB()" placeholder="Chercher un id étudiant">
     <form method="POST" action="/seance-details/{{$seance_id}}/validation">
         {{ csrf_field() }}
-        <table border="1">
-            <tr>
-                <th>Matricule</th>
-                <th>Nom</th>
-                <th>Prénom</th>
+        <table id="myTable" border="1">
+            <tr class="header">
+                <th>Etudiants</th>
                 <th>Présent(e) <br> <input type="checkbox" id="select-all" dusk="select-all"> Tout sélectionner</th>
             </tr>
             @foreach ($students as $student)
                 <tr>
-                    <td dusk='id_student'>{{$student->id}}</td>
-                    <td>{{$student->last_name}}</td>
-                    <td>{{$student->first_name}}</td>
+                    <td dusk='id_student'>{{$student->id}} {{$student->last_name}} {{$student->first_name}}</td>
                     <td><input type="checkbox" name="checklist[]" id="{{$student->id}}" dusk="{{$student->id}}"></td>
                     <td>
                         <button type="button"
