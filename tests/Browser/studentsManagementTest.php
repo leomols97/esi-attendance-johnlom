2c259344e650bfa6c0577bf656e3e1716f7eee5f
52006 <52006@etu.he2b.be>
2021-12-15 20:09:31 +0100

    Dusk Tests Updates, closes #52

--- tests/Browser/studentsManagementTest.php
+++ tests/Browser/studentsManagementTest.php
@@ -26,42 +26,33 @@ class studentsManagementTest extends DuskTestCase
         });
     }
 
-    // Ces tests ne passent pas et cassent la DB
-//    public function testDelete()
-//    {
-//        $this->browse(function (Browser $browser) {
-//            $browser->visit('/students_management')
-//                ->press('@delete2')
-//                ->acceptDialog()
-//                ->assertUrlIs('http://127.0.0.1:8000/students_management');
-//        });
-//    }
+    public function testAddStudent()
+   {
+       $this->browse(function (Browser $browser) {
+           $browser->visit('/students_management')
+               ->assertSee('Ajouter un étudiant')
+               ->type('@student_id', '52006')
+               ->type('@student_first_name', 'Olivier')
+               ->type('@student_last_name', 'Dyck')
+               ->type('@group_name', 'E11')
+               ->press('@add')
+               ->assertSeeIn("@id_student52006", "52006 Dyck Olivier");
+       });
+       Student::deleteStudent(52006);
+   }
+    
+   public function testDelete()
+   {
+       $this->browse(function (Browser $browser) {
+           $browser->visit('/students_management')
+               ->press('@delete2')
+               ->acceptDialog()
+               ->assertUrlIs('http://127.0.0.1:8000/students_management');
+       });
+       Student::add(2, Retest, Guillaume, E12);
+   }
 
-//    public function testAddStudent1()
-//    {
-//        $this->browse(function (Browser $browser) {
-//            $browser->visit('/students_management')
-//                ->assertSee('Ajouter un étudiant')
-//                ->type('@student_id', '52006')
-//                ->type('@student_first_name', 'Olivier')
-//                ->type('@student_last_name', 'Dyck')
-//                ->type('@group_name', 'E11')
-//                ->press('@add')
-//                ->assertSee("@id_student52006", "52006 Dyck Olivier");
-//        });
-//    }
-//
-//    public function testAddStudent2()
-//    {
-//        $this->browse(function (Browser $browser) {
-//            $browser->visit('/students_management')
-//                ->assertSee('Ajouter un étudiant')
-//                ->type('@student_id', '')
-//                ->type('@student_first_name', '')
-//                ->type('@student_last_name', '')
-//                ->type('@group_name', '')
-//                ->press('@add')
-//                ->assertSee("L'étudiant(e) n'a malheureusement pas pu être ajouté(e) !");
-//        });
-//    }
+   
+
+   
 }
