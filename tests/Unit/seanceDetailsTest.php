00c4ea5ec85dc8b6408cb99bd8fae2f977c61cf9
52006 <52006@etu.he2b.be>
2021-12-16 16:26:39 +0100

    Unit test Done, closes #62

--- tests/Unit/seanceDetailsTest.php
+++ tests/Unit/seanceDetailsTest.php
@@ -11,8 +11,9 @@ class seanceDetailsTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $this->assertTrue(true);
+    public function test_get_seance_details()
+    {   
+        $details = Seance::getSeanceDetails(1);
+        $this->assertEquals($details, array("PRJG5","KUmNxUEu18","2021-12-16 14:45:00"));
     }
 }
