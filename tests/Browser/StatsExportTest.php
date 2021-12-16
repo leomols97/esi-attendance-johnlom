5907bc917ee9f133c23dc899f8edd642b3d6ea8d
leomols97 <leomols@hotmail.com>
2021-12-16 03:53:26 +0100

    Woups... forgot to fix a conflict #47

--- tests/Browser/StatsExportTest.php
+++ tests/Browser/StatsExportTest.php
@@ -38,11 +38,7 @@ class StatsExportTest extends DuskTestCase
                     ->type('name', 'goku')
                     ->select('extension', 'csv')
                     ->press('Exporter')
-<<<<<<< HEAD
                     ->assertUrlIs('https://esi-attendance-johnlom.herokuapp.com/export_stats_presences'); // POST is handled by the same page
-=======
-                    ->assertUrlIs('http://esi-attendance-johnlom.herokuapp.com/export_stats_presences'); // POST is handled by the same page
->>>>>>> master
         });
     }
 
