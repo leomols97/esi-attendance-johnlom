8c1d963a442e556eaeddb962133b3f0f2e4ff641
52006 <52006@etu.he2b.be>
2021-12-16 04:07:39 +0100

    first iteration, related to #54

--- tests/Browser/PresenceSaverTest.php
+++ tests/Browser/PresenceSaverTest.php
@@ -14,11 +14,11 @@ class PresenceSaverTest extends DuskTestCase
     public function test_check_all()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/students_management')
+            $browser->visit('/seance-details/1')
                     ->check("@select-all")
                     ->assertChecked("@select-all")
-                    ->assertChecked("@0")
-                    ->assertChecked("@1");
+                   // ->assertChecked("@0")
+                    ->assertChecked("@10000");
         });
     }
 
@@ -28,10 +28,10 @@ class PresenceSaverTest extends DuskTestCase
     public function test_validate()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/students_management')
+            $browser->visit('/seance-details/1')
                     ->check("@select-all")
                     ->press("Valider les présences")
-                    ->assertUrlIs("http://127.0.0.1:8000/students/1/validation")
+                    ->assertUrlIs("http://esi-attendance-johnlom.herokuapp.com/exportStats/students/1/validation")
                     ->assertPresent("@success");
         });
     }
